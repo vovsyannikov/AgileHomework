b15dd61804a4bbfc110cdd87e79ad8a8cc801d49
Виталий <its.vint@ya.ru>
2020-11-27 21:47:50 +0300

    Initial Commit

---
+++ AgileHomework/AgileHomeworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  AgileHomeworkApp.swift
+//  AgileHomework
+//
+//  Created by Виталий Овсянников on 27.11.2020.
+//
+
+import SwiftUI
+
+@main
+struct AgileHomeworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
